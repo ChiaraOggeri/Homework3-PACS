1117c399a24878d710c1181847bcc2abcdfd0f75
ChiaraOggeri <chiara.oggeribreda@gmail.com>
2020-12-10 11:40:51 +0100

    Update gradient_reversal_example.py

--- gradient_reversal_example.py
+++ gradient_reversal_example.py
@@ -25,11 +25,37 @@ class ReverseLayerF(Function):
 
 
 class RandomNetworkWithReverseGrad(nn.Module):
-    def __init__(self, **kwargs):
+    def __init__(self, num_classes=7):
         super(RandomNetworkWithReverseGrad, self).__init__()
-        self.features = nn.Sequential(...)
-        self.classifier = nn.Sequential(...)
-        self.dann_classifier = nn.Sequential(...)
+        self.features = nn.Sequential( nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),)
+        self.classifier = nn.Sequential(nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes),
+           )
+        self.dann_classifier = nn.Sequential( nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, 2),
+          )
 
     def forward(self, x, alpha=None):
         features = self.features
@@ -39,10 +65,10 @@ class RandomNetworkWithReverseGrad(nn.Module):
         if alpha is not None:
             # gradient reversal layer (backward gradients will be reversed)
             reverse_feature = ReverseLayerF.apply(features, alpha)
-            discriminator_output = ...
+            discriminator_output = slef.dann_classifier
             return discriminator_output
         # If we don't pass alpha, we assume we are training with supervision
         else:
             # do something else
-            class_outputs = ...
+            class_outputs = self.classifier
             return class_outputs
